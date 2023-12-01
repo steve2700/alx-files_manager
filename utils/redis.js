1e921a316eb57cb9a5dcc06850c5d294061d8663
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-12-01 18:03:55 +0100

    redis client created with 1 issue to fix

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,50 @@
-const redis = require('redis);
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display any error of the redis client in the console
+    this.client.on('error', (err) => {
+      console.error(`Redis Client Error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    try {
+      const value = await getAsync(key);
+      return value;
+    } catch (error) {
+      console.error(`Error getting value from Redis: ${error}`);
+      throw error;
+    }
+  }
+
+  async set(key, value, durationInSeconds) {
+    try {
+      await this.client.set(key, value, 'EX', durationInSeconds);
+    } catch (error) {
+      console.error(`Error setting value in Redis: ${error}`);
+      throw error;
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (error) {
+      console.error(`Error deleting value from Redis: ${error}`);
+      throw error;
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;

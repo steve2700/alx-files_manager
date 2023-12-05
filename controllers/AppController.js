8b14c5775d3a13e2ab07badca6ba23c5635c1723
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-12-05 17:32:20 +0100

    fixing task 2 error

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,15 +2,19 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
   }
 
-  static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({ users: usersCount, files: filesCount });
   }
 }
 
-module.exports = AppController;
+export default AppController;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import router from './routes/index';
 
-const port = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
+app.use('/api', router); // Assuming your routes are under '/api'
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
